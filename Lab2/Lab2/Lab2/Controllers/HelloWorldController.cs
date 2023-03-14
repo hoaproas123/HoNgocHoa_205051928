497b6962010b39f3af499f6935f81971083739a4
hoaproas123 <hoaproas1@gmail.com>
2023-03-14 15:38:27 +0700

    first commit

---
+++ Lab2/Lab2/Lab2/Controllers/HelloWorldController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace Lab2.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+        public string Index()
+        {
+            return "This is my default action...";
+        }
+        //
+        // GET: /HelloWorld/Welcome/
+        //public string Welcome(string name, int numTimes = 1)
+        //{
+        //    return HtmlEncoder.Default.Encode($"Hello {name}, NumTimes is: {numTimes}");
+        //}
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+
+    }
+}
---
+++ Lab2/Lab2/Lab2/Data/Lab2Context.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Lab2.Models;
+
+namespace Lab2.Data
+{
+    public class Lab2Context : DbContext
+    {
+        public Lab2Context (DbContextOptions<Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Lab2.Models.Movie> Movie { get; set; } = default!;
+    }
+}
