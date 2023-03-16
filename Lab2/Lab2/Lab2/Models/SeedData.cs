22f9fcd9d0e222510cdceabd5807f4d4b63e18ac
hoaproas123 <hoaproas1@gmail.com>
2023-03-16 14:36:11 +0700

    Lab4

--- Lab2/Lab2/Lab2/Models/SeedData.cs
+++ Lab2/Lab2/Lab2/Models/SeedData.cs
@@ -22,13 +22,15 @@ namespace Lab2.Models
                     Title = "When Harry Met Sally",
                     ReleaseDate = DateTime.Parse("1989-2-12"),
                     Genre = "Romantic Comedy",
-                    Price = 7.99M
+                    Price = 7.99M,
+                    Rating = "R"
                 },
                 new Movie
                 {
                     Title = "Ghostbusters ",
                     ReleaseDate = DateTime.Parse("1984-3-13"),
                     Genre = "Comedy",
+                    Rating = "R",
                     Price = 8.99M
                 },
                 new Movie
@@ -36,6 +38,7 @@ namespace Lab2.Models
                     Title = "Ghostbusters 2",
                     ReleaseDate = DateTime.Parse("1986-2-23"),
                     Genre = "Comedy",
+                    Rating = "R",
                     Price = 9.99M
                 },
                 new Movie
@@ -43,6 +46,7 @@ namespace Lab2.Models
                     Title = "Rio Bravo",
                     ReleaseDate = DateTime.Parse("1959-4-15"),
                     Genre = "Western",
+                    Rating = "R",
                     Price = 3.99M
                 }
                 );
